f47a787f6a726d2c3351f63613460fa6d803ea89
Celine <celinesuarez7@gmail.com>
2014-11-06 15:46:28 -0600

    updating directories

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'localhost/CodeIgniter/CodeIgniter-2.2-stable/';
+$config['base_url']	= 'localhost/IN_CodeIgniter';
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/views/pages/home.php
@@ -0,0 +1,3 @@
+<?php
+echo "should work";
+?>
